3d8c45384c5d7a24b485fac4fd2a9d3e635d0539
Svenw <74722783+svenwlive@users.noreply.github.com>
2024-10-11 09:29:37 +0300

    update

--- script.js
+++ script.js
@@ -2,18 +2,26 @@ console.info("hiya! your javascript is working!")
 console.info("^_^")
 
 async function download() {
-    const api = "https://api.breakingbadquotes.xyz/v1/quotes";
+    let quoteNum = prompt("How many quotes do you want?")
+
+    let api = "https://api.breakingbadquotes.xyz/v1/quotes/";
+    api += quoteNum;
+    console.info("Getting " + quoteNum + " quotes from api")
     const response = await fetch(api);
     const data = await response.json();
-    const quoteText = document.getElementById("quote");
-    const authorText = document.getElementById("author");
+    // const quoteText = document.getElementById("quote");
+    // const authorText = document.getElementById("author");
+    const quoteBox = document.querySelector('.quotebox');
 
-    quoteText.innerHTML = "";
+    quoteBox.innerHTML = "";
     for (const item of data) {
+        // const quote = item.quote;
+        // const author = item.author;
+        // quoteText.innerHTML += quote;
+        // authorText.innerHTML += "~ " + author;
         const quote = item.quote;
         const author = item.author;
-        quoteText.innerHTML = quote;
-        authorText.innerHTML = "~ " + author;
+        quoteBox.innerHTML +='<p id="quote" class="quote">' + quote + '</p>' + '<p id="author" class="author">~ ' + author + '</p>'
     }
     console.log("finished download")
 }
